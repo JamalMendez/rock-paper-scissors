dc65ab494df40475a9aceac50204e70909978441
Jamal Mendez <jamalmendezfeliz@hotmail.com>
2024-05-01 01:08:11 -0400

    Addition of the function getHumanChoice
    This function allows the user to make a choice between rock, paper,
    and scissors. When the function is called, it prompts the user to
    enter one of the three possible options, validates that it is
    written correctly, and then returns the choice.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,22 @@
+const getComputerChoice = () => {
+    const random = Math.floor(Math.random() * 3);
+    if(random === 0){
+        return "rock";
+    }else if(random === 1) {
+        return "paper";
+    }else {
+        return "scissors";
+    }
+};
+
+const getHumanChoice = () => {
+    while(true){
+        const choice = prompt("Select a choice between: rock, paper or scissors.").toLowerCase();
+        if(choice === ("rock" || choice === "paper" || choice === "scissors")){
+            return choice;
+        }
+    }    
+};
+
+console.log(getHumanChoice());
+console.log(getComputerChoice());
